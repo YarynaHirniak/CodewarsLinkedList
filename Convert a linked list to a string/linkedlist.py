1e0eaa50a06c432ecec833467adbbec91925d31a
YarynaHirniak <149358891+YarynaHirniak@users.noreply.github.com>
2024-04-08 12:06:02 +0300

    code of linked list to a string

---
+++ Convert a linked list to a string/linkedlist.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+def stringify(node):
+    head = node
+    res = ''
+    while head is not None:
+        res+=str(head.data)
+        res+=' -> '
+        head = head.next
+    res+='None'
+    return res
